4086320758e3f75c083aca2bf15555f5805164ab
Erykov9 <szczepanekeryk@gmail.com>
2023-03-30 11:03:40 +0200

    change package.json

--- db.js
+++ db.js
@@ -4,19 +4,20 @@ require('dotenv').config();
 const dbConnect = async () => {
   mongoose
     .connect(
-      process.env.DB_URL,
+      'mongodb+srv://Erykov9:test123@musicwebsitedb.ihzwpqr.mongodb.net/portfolio?retryWrites=true&w=majority',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
   )
   .then(() => {
-    console.log('Successfully connected to DATABASE: ' + process.env.DB_URL.split('/')[3])  
+    console.log('Successfully connected to DATABASE: ' + 'mongodb+srv://Erykov9:test123@musicwebsitedb.ihzwpqr.mongodb.net/portfolio?retryWrites=true&w=majority'.split('/')[3])  
   })
   .catch((error) => {
     console.log('Unable to connect to DATABASE');
     console.error(error);
   })
 };
+
 
 module.exports = dbConnect;
